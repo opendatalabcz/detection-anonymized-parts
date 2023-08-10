c9672baa47d7aa8a65140a51a767ecbf972b1633
Lukas Salak <lukas.salak@profinit.eu>
2023-08-10 23:51:54 +0200

    Removed old api

--- implementation/DAPP/API/Requests/AnalyzeRequest.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace API.Requests;
-/// <summary>
-/// A request for a file location
-/// </summary>
-/// <param name="FileLocation"> The location of the file</param>
-public record AnalyzeRequest(string FileLocation, bool ReturnImages = false);
--- implementation/DAPP/API/Usings.cs
+++
@@ -1,9 +0,0 @@
-﻿global using API.Requests;
-global using API.Services;
-global using API.Errors;
-global using DAPPAnalyzer.Services;
-global using DAPPAnalyzer.Models;
-global using ErrorOr;
-
-global using System.Diagnostics;
-global using System.Net;
